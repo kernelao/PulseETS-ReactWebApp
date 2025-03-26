8d9c0a6ca2dfbe347898a8050f8ba079d5a20ef2
farkunda <farkunda.wahedi.1@ens.etsmtl.ca>
2025-03-26 14:50:13 -0400

    Settings

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './app.css';
 import AppRouter from './routes/AppRouter';
 import AuthProvider from './context/AuthContext';
+import Settings from './pages/utilisateur/Settings.jsx'
 
 function App() {
   return (
     <AuthProvider>
       <AppRouter />
+      <Settings/>
     </AuthProvider>
   );
 }
