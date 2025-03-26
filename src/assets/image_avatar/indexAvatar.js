248ff795407f17ca896fdbd95a09aee090463913
jimich514 <jimich514@gmail.com>
2025-03-26 14:39:13 -0400

    modif profil recompenses

---
+++ src/assets/image_avatar/indexAvatar.js
@@ -0,0 +1,43 @@
+import defaultavatar from './defaultavatar.svg';
+import avatarchandailrose from './avatarchandailrose.svg';
+import avatarchapeaugris from './avatarchapeaugris.svg';
+import avatarchapeaushades from './avatarchapeaushades.svg';
+import avatarcheveuxbleux from './avatarcheveuxbleux.svg';
+import avatarcheveuxroux from './avatarcheveuxroux.svg';
+import avatargreenblack from './avatargreenblack.svg';
+import avatarhatmauve from './avatarhatmauve.svg';
+import avatarlunettesfancy from './avatarlunettesfancy.svg';
+import avatarroussepurplehat from './avatarroussepurplehat.svg';
+import avatarshades from './avatarshades.svg';
+import avatarsuits from './avatarsuits.svg';
+import SPECIALbatman from './SPECIALbatman.svg';
+import SPECIALcr7 from './SPECIALcr7.svg';
+import SPECIALfemalesuperhero from './SPECIALfemalesuperhero.svg';
+import SPECIALironman from './SPECIALironman.svg';
+import SPECIALjoker from './SPECIALjoker.svg';
+import VIPavatargreenblack from './VIPavatargreenblack.svg';
+import VIPavatarshadescheveuxvert from './VIPavatarshadescheveuxvert.svg';
+
+const AVATAR = {
+    defaultavatar,
+    avatarchandailrose,
+    avatarchapeaugris,
+    avatarchapeaushades,
+    avatarcheveuxbleux,
+    avatarcheveuxroux,
+    avatargreenblack,
+    avatarhatmauve,
+    avatarlunettesfancy,
+    avatarroussepurplehat,
+    avatarshades,
+    avatarsuits,
+    SPECIALbatman,
+    SPECIALcr7,
+    SPECIALfemalesuperhero,
+    SPECIALironman,
+    SPECIALjoker,
+    VIPavatargreenblack,
+    VIPavatarshadescheveuxvert
+};
+
+export default AVATAR;
