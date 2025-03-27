01d7e85d5f6cf13814113f8dd47fd2a33bf0baa1
farkunda <farkunda.wahedi.1@ens.etsmtl.ca>
2025-03-26 21:52:19 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './app.css';
 import AppRouter from './routes/AppRouter';
 import AuthProvider from './context/AuthContext';
-import Settings from './pages/utilisateur/Settings.jsx'
 
 function App() {
   return (
