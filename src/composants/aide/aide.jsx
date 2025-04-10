ff655e0bcb573bd5bc00eda531faee29f91413ee
farkunda <farkunda.wahedi.1@ens.etsmtl.ca>
2025-04-10 19:36:04 -0400

    Reapply "aide frontend"
    This reverts commit e3bb88f920c426364747377ff7e3c487870fc565.

--- src/composants/aide/aide.jsx
+++ src/composants/aide/aide.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import './aide.css';
 
-const aide = () => {
+const Aide = () => {
   return (
     <div className="aideMain">
       <h2>Support &amp; assistance</h2>
       <div className="aideContact">
         <h3>Nous contacter</h3>
+
+
+        <p>Veuillez saisir votre courriel</p>
+        <input 
+          type="email" 
+          id="aideEmail" 
+          placeholder="Votre courriel" 
+        />
+
         <p>Signalez un bug ou posez une question</p>
         <textarea 
           className="aideInput" 
           placeholder="Écrire votre message..." 
         />
+
         <button className="aideBouton">Envoyer</button>
       </div>
     </div>
   );
 };
 
-export default aide;
+export default Aide;
