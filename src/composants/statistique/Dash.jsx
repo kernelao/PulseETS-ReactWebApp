9ae6e1b3a131e89ff02bf87c7d2d84c340a1ef18
ArisaHalluni <arisa.halluni.1@ens.etsmtl.ca>
2025-04-14 20:12:50 -0400

    Username greeting
    Greeting du user

--- src/composants/statistique/Dash.jsx
+++ src/composants/statistique/Dash.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Greeting from './Greeting';
 import Stats from './Stats';
+import axios from './../../api/Axios';
 import './dash.css';
 
 const Dash = () => {
-    return (
-      <div className="dashboard-container">
-        <Greeting username="User" /> 
-        <Stats />
-      </div>
-    );
-  };
+  const [username, setUsername] = useState('User');
+
+  useEffect(() => {
+    const fetchUsername = async () => {
+      try {
+        const res = await axios.get('/dashboard');
+        setUsername(res.data.user?.username || 'User');
+      } catch (err) {
+        console.error('Erreur en récupérant le nom d’utilisateur:', err);
+      }
+    };
+
+    fetchUsername();
+  }, []);
+
+  return (
+    <div className="dashboard-container">
+      <Greeting username={username} />
+      <Stats />
+    </div>
+  );
+};
 
 export default Dash;
